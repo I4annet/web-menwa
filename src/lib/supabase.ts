39f17feee8f76e3109f347b678b8c58dbe70a75c
Ivansyah Eka Oktaviadi Santoso <ivansyahsantoso@gmail.com>
2026-07-08 10:01:21 +0700

    buat ulang

--- src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
-
-// Initialize Supabase only if credentials are provided in .env
-export const supabase = supabaseUrl && supabaseAnonKey
-  ? createClient(supabaseUrl, supabaseAnonKey)
-  : null;
