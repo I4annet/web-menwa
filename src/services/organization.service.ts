a49e92ab2ef8bb23a48f355fbbbeb4dc9a2f2212
Ivansyah Eka Oktaviadi Santoso <ivansyahsantoso@gmail.com>
2026-07-09 11:12:02 +0700

    menampilkan data ke dari database ke halaman web🛠️

--- src/services/organization.service.ts
+++ src/services/organization.service.ts
@@ -10,16 +10,19 @@ import { OrganizationMember } from "../types/organization";
 
 export async function getOrganizationMembers() {
     const q = query(
-        collection(db, "organisasi"),
+        collection(db, "members"),
         orderBy("order", "asc")
     );
 
     const snapshot = await getDocs(q);
+
+    console.log("Jumlah data", snapshot.size);
 
     const members: OrganizationMember[] = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...(doc.data() as Omit<OrganizationMember, "id">),
     }));
 
+    console.log(members)
     return members;
 }
